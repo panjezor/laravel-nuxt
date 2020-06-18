2e9cf24eecad4eedee127d3fa89ac6f5b28238cf
panjezor <filipjancieslik@gmail.com>
2020-06-18 22:46:46 +0100

    this needs to be fixed

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,10 +17,9 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
-
-            //return response()->json(['error' => 'Already authenticated.'], 400);
-        }
+//        if (Auth::guard($guard)->check()) {
+//            return response()->json(['error' => 'Already authenticated.'], 400);
+//        }
 
         return $next($request);
     }
